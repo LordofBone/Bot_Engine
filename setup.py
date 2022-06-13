5377b19e4c776083b1c985c02035968d7fccf180
314Reactor <34923578+LordofBone@users.noreply.github.com>
2022-06-13 22:06:32 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='BotEngine',
+    version='',
+    packages=['', 'config', 'functions', 'utils', 'ml'],
+    install_requires=[
+        'Chatbot_8',
+        'pillow',
+        'psycopg2-binary',
+        'python-dateutil',
+        'tqdm',
+        'PyYAML',
+        'setuptools',
+        'playsound',
+        'espeakng',
+        'nltk',
+        'matplotlib',
+        'markovify',
+        'tensorflow',
+        'tensorflow-gpu'
+    ],
+    url='',
+    license='',
+    author='314Reactor',
+    author_email='mikedarby@rocketmail.com',
+    description='Installation for using Bot Engine as a part of another project.'
+)

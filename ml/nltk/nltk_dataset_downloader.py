691879097d768769c60da2dce544c0f0388b7fae
314Reactor <34923578+LordofBone@users.noreply.github.com>
2022-06-14 19:11:09 +0100

    Added 'omw-1.4' to downloads, seems to be needed when running for first time

--- ml/nltk/nltk_dataset_downloader.py
+++ ml/nltk/nltk_dataset_downloader.py
@@ -5,6 +5,7 @@ def download_twitter_datasets():
     nltk.download('twitter_samples', download_dir=dataset_path_root)
     nltk.download('wordnet', download_dir=dataset_path_root)
     nltk.download('stopwords', download_dir=dataset_path_root)
+    nltk.download('omw-1.4', download_dir=dataset_path_root)
 
 
 if __name__ == "__main__":
